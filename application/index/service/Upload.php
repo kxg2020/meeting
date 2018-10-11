4321aa3c3fbf571378246a21df0df9d7d8f11fa6
zhangtao <zhangtao@xinchao.mobi>
2018-10-11 16:20:51 +0800

    update

--- application/index/service/Upload.php
+++ application/index/service/Upload.php
@@ -7,17 +7,17 @@ class Upload{
     use Singleton;
     private $result   = [];
     private $savePath = "static/uploads/";
-    private $size     = 1024 * 1024 * 20;
+    private $fileSize     = 1024 * 1024 * 20;
     private $extension= 'bmp,gif,jpg,jpeg,pic,png,tif,wmv,asf,asx,rm, rmvb,mp4,mp3,3gp,mov,m4v,avi,dat,mkv,flv,vod,txt,doc,xls,ppt,docx,xlsx,pptx';
 
     public function uploadFile(){
         if($this->checkPath()){
             $file = Request::file("file");
             $root = Url::build("index/index");
-            $info = $file->validate(['size'=>$this->size,'ext'=>$this->extension])->move($this->savePath,"");
+            $info = $file->validate(['size'=>$this->fileSize,'ext'=>$this->extension])->move($this->savePath,"");
             if($info){
                 $this->result["file_url"]   = $root.$this->savePath.DIRECTORY_SEPARATOR.$info->getSaveName();
-                $this->result["file_name"] = $info->getFilename();
+                $this->result["file_name"]  = $info->getFilename();
                 return json(["status" => true,"data" =>$this->result,"msg"=>"success"]);
             }else{
                 return json(["status" => false,"data" =>$this->result,"msg" => $file->getError()]);
