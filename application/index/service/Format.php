6e6e529e6bfe06492dc933170f11e07a91f91807
zhangtao <zhangtao@xinchao.mobi>
2018-10-12 10:41:00 +0800

    update

--- application/index/service/Format.php
+++ application/index/service/Format.php
@@ -2,6 +2,7 @@
 namespace app\index\service;
 
 use think\Db;
+use think\facade\Log;
 
 class Format{
     use Singleton;
@@ -80,20 +81,22 @@ class Format{
 
     public function votes(){
         $this->commonFiles();
-        $infoId = Db::name("meeting_record_info")->insert($this->meetingInfo);
+        $infoId = Db::name("meeting_record_info")->insertGetId($this->meetingInfo);
         if(isset($this->issue["votes"]) && $this->issue["votes"]){
             foreach($this->issue["votes"] as $key => $value){
-                $insertMeetingVote["vote_name"] = $value["title"];
                 $insertMeetingVote["meeting_info_id"] = $infoId;
                 if($value["items"]){
                     foreach($value["items"] as $item){
+                        $fileId = [];
+                        $insertMeetingVote["vote_name"] = $item["value"];
                         if(isset($item["files"]) && !empty($item["files"])){
                             foreach($item["files"] as $file){
                               $fileId[] =  $this->createFile($file);
                             }
                         }
+                        $singleVoteId = implode(",",$fileId);
                         $insertMeetingVote["vote_choose"] = $item["value"];
-                        $insertMeetingVote["file_id"]     = implode(",",$fileId);
+                        $insertMeetingVote["file_id"]     = $singleVoteId;
                         $result = Db::name("meeting_votes")->insertGetId($insertMeetingVote);
                         if(!$result){
                             $this->commit = false;
@@ -107,8 +110,8 @@ class Format{
 
     private function createFile($params){
         $insert = [
-            "url"       => isset($params["fil_url"])  ? $params["fil_url"]  : "",
-            "file_name" => isset($params["fil_name"]) ? $params["fil_name"] : "",
+            "url"       => isset($params["file_url"])  ? $params["file_url"]  : "",
+            "file_name" => isset($params["file_name"]) ? $params["file_name"] : "",
         ];
         $result = Db::name("meeting_file")->insertGetId($insert);
         if($result){
