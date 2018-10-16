c0971a135b17fb369fe41707a81b99d4c6660851
zhangtao <zhangtao@xinchao.mobi>
2018-10-16 10:12:18 +0800

    update

--- application/index/model/MeetingRecordInfo.php
+++ application/index/model/MeetingRecordInfo.php
@@ -3,6 +3,7 @@ namespace app\index\model;
 use app\index\service\Enum;
 use app\index\service\MeetingVotes;
 use app\index\service\Singleton;
+use app\index\service\Tool;
 use think\Cache;
 use think\Db;
 
@@ -11,16 +12,8 @@ class MeetingRecordInfo extends Base{
     private $imageExt = ["jpg,jpeg,png,gif"];
 
     public function meetingIssueInfo($issueId){
-        $meetingInfo = \think\facade\Cache::get("$issueId-info");
-        if($meetingInfo){
-            $meetingInfo = \app\index\service\Tool::getInstance()
-                ->jsonDecode($meetingInfo);
-            unset($meetingInfo["data"]["join_user"]);
-        }else{
-            $meetingInfo = $this->singleIssueInfo($issueId);
-            unset($meetingInfo["data"]["join_user"]);
-        }
-
+        $meetingInfo = $this->singleIssueInfo($issueId);
+        return $this->returnResponse($meetingInfo["data"]);
     }
 
     private function singleIssueInfo($issueId){
@@ -32,18 +25,18 @@ class MeetingRecordInfo extends Base{
             ->leftJoin("meeting_political b","a.type = b.id")
             ->where(["a.id" => $issueId])
             ->find();
-
         if($issueDetail){
             $result["content"] = $issueDetail["content"];
             $result["issue_name"] = $issueDetail["title"];
             $result["issue_short_name"] = $issueDetail["short_name"];
+            $result["images"] = [];
+            $result["files"]  = [];
+            $result["option"] = [];
             // 查询议题的文件列表
             if(isset($issueDetail["file_id"]) && !empty($issueDetail["file_id"])){
                 // 查询文件
                 $files = MeetingFile::getInstance()->issueFileList($issueDetail["file_id"]);
                 if($files["data"]){
-                    $result["images"] = [];
-                    $result["files"]  = [];
                     foreach($files["data"] as $file){
                         $ext = substr($file["file_name"],strpos($file["file_name"],"."));
                         if(in_array($ext,$this->imageExt)){
@@ -58,53 +51,62 @@ class MeetingRecordInfo extends Base{
                             ];
                         }
                     }
-                    // 投票选项
-                    $votes = \app\index\model\MeetingVotes::getInstance()->votesList($issueId);
+                    $result = $this->classify($issueId,$issueDetail,$result);
+                    return $this->returnResponse($result);
+                }
+            }
+            return $this->returnResponse($result);
+        }
+        return $this->returnResponse();
+    }
 
-
-                    if($votes){
-                        // 判断议题类型
-                        switch($issueDetail["short_name"]){
-                            case Enum::READ:
-                                $result["votes"] = [];
-                                break;
-                            case Enum::BALLOT:
-                                if(isset($votes["data"]) && !empty($votes["data"])){
-                                    array_walk($votes["data"],function($value) use (&$result){
-                                        $ballotFile = MeetingFile::getInstance()
-                                            ->votesFileList($value["file_id"]);
-                                        $result["option"][] = [
-                                            "vote_name" => $value["vote_name"],
-                                            "options"   => explode(",",$value["vote_choose"]),
-                                            "file"      => $ballotFile
-                                        ];
-                                    });
-                                }
-                                break;
-                            case Enum::VOTE:
-                                if(isset($votes["data"]) && !empty($votes["data"])){
-                                    foreach($votes["data"] as $index => $item){
-                                        // 查询投票文件
-                                        $voteFile = MeetingFile::getInstance()
-                                            ->votesFileList($item["file_id"]);
-                                       
-                                    }
-                                }else{
-                                    $result["option"] = [];
-                                }
-                                var_dump($result);die;
-                                break;
-                        }
-
+    private function classify($issueId,$issueDetail,&$result){
+        // 投票选项
+        $votes = \app\index\model\MeetingVotes::getInstance()->votesList($issueId);
+        $function = function ($result) use ($votes){
+            $voteTitle    = [];
+            $subjectIndex = 0;
+            if(isset($votes["data"]) && !empty($votes["data"])){
+                foreach ($votes["data"] as $index => $item){
+                    $file = MeetingFile::getInstance()->votesFileList($item["file_id"]);
+                    if(in_array($item["vote_name"],$voteTitle)){
+                        $result["option"][$subjectIndex]["options"][] = [
+                            "title"       => $item["vote_name"],
+                            "choose_name" => $item["vote_choose"],
+                            "file"        => $file["data"]
+                        ];
                     }else{
-                        $result["votes"] = [];
+                        $voteLength = array_push($voteTitle,$item["vote_name"]);
+                        $subjectIndex = $voteLength - 1;
+                        $result["option"][$subjectIndex]["options"][] = [
+                            "title"       => $item["vote_name"],
+                            "choose_name" => $item["vote_choose"],
+                            "file"        => $file["data"],
+                        ];
                     }
                 }
             }else{
-                $result["image"] = [];
-                $result["file"]  = [];
+                $result["option"] = [];
             }
+            return $result;
+        };
+
+        if($votes){
+            switch($issueDetail["short_name"]){
+                case Enum::READ:
+                    $result["option"] = [];
+                    break;
+                case Enum::BALLOT:
+                   $result =  $function($result);
+                    break;
+                case Enum::VOTE:
+                    $result =  $function($result);
+                    break;
+            }
+
+        }else{
+            $result["votes"] = [];
         }
-        return $this->returnResponse();
+        return $result;
     }
 }
