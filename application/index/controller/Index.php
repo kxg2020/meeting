23bd4e4387f139058cfb61c20e94e0dfee959362
zhangtao <zhangtao@xinchao.mobi>
2018-10-16 17:31:26 +0800

    update

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -7,6 +7,7 @@ use app\index\service\Jwt;
 use app\index\service\WeChat;
 use think\Exception;
 use think\facade\Config;
+use think\facade\Log;
 use think\facade\Request;
 use think\facade\Url;
 
@@ -38,6 +39,7 @@ class Index extends Base{
                 }
             }else{
                 $this->redirect = Request::url(true);
+                Log::write("request url :".$this->redirect);
                 $redirect = sprintf($this->authApi,$this->companyId,$this->redirect,$this->agentId);
                 return redirect($redirect);
             }
