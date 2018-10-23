36c3c61c9eb1c184e414fadbe74aa2108c0a6d11
gaow <1225039937@qq.com>
2018-10-23 21:22:18 +0800

    vote post data

--- resources/js/modules/meeting/MeetingRecordIssue.vue
+++ resources/js/modules/meeting/MeetingRecordIssue.vue
@@ -14,34 +14,21 @@
           <FileList :files="issue.files" :show-remove="false" :show-download="true" :preview="true"></FileList>
         </div>
       </div>
-      <div v-if="bjVotes.length > 0" class="block">
+      <div v-if="issue.option.length > 0" class="block">
         <div class="item-header">
-          <i class="fa fa-hand-paper-o"></i><span style="margin-left: 1em;">表决</span>
+          <i class="fa fa-hand-paper-o"></i><span style="margin-left: 1em;">{{issue.issue_short_name == 'bj' ? '表决' : '投票'}}</span>
         </div>
-        <div class="vote-item" v-for="(bj, bjIndex) in bjVotes" :key="'bj' + bjIndex">
-          <div class="vote-title">{{bj.title}}</div>
-          <el-radio-group v-model="bjVoteSelect[bjIndex]" size="mini">
-            <el-radio :label="optionIndex" border v-for="(option, optionIndex) in bj.options"
-                      :key="'bji' + optionIndex">{{option}}
-            </el-radio>
-          </el-radio-group>
-        </div>
-      </div>
-      <div v-if="tpVotes.length > 0" class="block">
-        <div class="item-header">
-          <i class="fa fa-hand-paper-o"></i><span style="margin-left: 1em;">投票</span>
-        </div>
-        <div class="vote-item" v-for="(tp, tpIndex) in tpVotes" :key="'tp' + tpIndex">
-          <div class="vote-title">{{tp.title}}</div>
-          <div v-for="(option, optionIndex) in tp.options" class="vote-item-notice" :key="'tpf' + optionIndex" v-if="option.files.length > 0">
-            选项{{(optionIndex + 1).ConvertToChinese()}} <span style="color: #409EFF;">{{option.choose_name}}</span>
+        <div class="vote-item" v-for="(vote, voteIndex) in issue.option" :key="'o' + voteIndex">
+          <div class="vote-title">{{vote.title}}</div>
+          <div v-for="(item, itemIndex) in vote.items" class="vote-item-notice" :key="'i' + itemIndex" v-if="item.files.length > 0">
+            选项{{(itemIndex + 1).ConvertToChinese()}} <span style="color: #409EFF;">{{item.value}}</span>
             <div>
-              <FileList :files="option.files" :show-remove="false" :show-download="true" :preview="true"></FileList>
+              <FileList :files="item.files" :show-remove="false" :show-download="true" :preview="true"></FileList>
             </div>
           </div>
-          <el-radio-group v-model="tpVoteSelect[tpIndex]" size="mini">
-            <el-radio :label="optionIndex" border v-for="(option, optionIndex) in tp.options"
-                      :key="'tpi' + optionIndex">{{option.choose_name}}
+          <el-radio-group v-model="voteSelects[voteIndex]" size="mini">
+            <el-radio :label="itemIndex" border v-for="(item, itemIndex) in vote.items"
+                      :key="'iv' + itemIndex">{{item.value}}
             </el-radio>
           </el-radio-group>
         </div>
@@ -65,10 +52,7 @@
     data() {
       return {
         issue: null,
-        bjVotes: [],
-        bjVoteSelect: [],
-        tpVotes: [],
-        tpVoteSelect: [],
+        voteSelects: [],
         submitLoading: false
       }
     },
@@ -89,40 +73,11 @@
         let _this = this
         _this.axios.get('/meetingRecord/detail/' + id).then(res => {
           _this.issue = res.data
-          if (_this.issue.issue_short_name == 'yz') {
-
+          let voteSelects = []
+          for (let i in _this.issue.option) {
+            voteSelects.push(0)
           }
-          if (_this.issue.issue_short_name == 'bj') {
-            let bjVotes = []
-            for (let option of _this.issue.option) {
-              let vote = {}
-              vote.title = option.options[0].title
-              vote.choose_id = option.options[0].choose_id
-              vote.options = option.options[0].choose_name.split(',')
-              bjVotes.push(vote)
-              _this.bjVoteSelect.push(0)
-            }
-            _this.bjVotes = bjVotes
-          }
-          if (_this.issue.issue_short_name == 'tp') {
-            let tpVotes = []
-            for (let option of _this.issue.option) {
-              let vote = {}
-              let options = []
-              vote.title = option.options[0].title
-              for (let i in option.options) {
-                options.push({
-                  files: option.options[i].file,
-                  choose_name: option.options[i].choose_name,
-                  choose_id: option.options[i].choose_id
-                })
-              }
-              vote.options = options
-              tpVotes.push(vote)
-              _this.tpVoteSelect.push(0)
-            }
-            _this.tpVotes = tpVotes
-          }
+          _this.voteSelects = voteSelects
         }).catch(err => {
         })
       },
@@ -131,27 +86,11 @@
         let postData = {}
         postData.issue_id = _this.issue.issue_id
         postData.issue_short_name = _this.issue.issue_short_name
-        if (_this.issue.issue_short_name == 'bj') {
-          let votes = []
-          for (let i in _this.bjVotes) {
-            votes.push({
-              choose_id: _this.bjVotes[i].choose_id,
-              select_index: _this.bjVoteSelect[i]
-            })
-          }
-          postData.votes = votes
-        }
-        if (_this.issue.issue_short_name == 'tp') {
-          let votes = []
-          for (let i in _this.tpVoteSelect) {
-            votes.push(_this.tpVotes[i].options[_this.tpVoteSelect[i]].choose_id)
-          }
-          postData.votes = votes
-        }
+        postData.votes = _this.voteSelects
         _this.axios.post('/userVotes/create', postData).then(res => {
           if (res.status) {
             _this.$toast(res.msg)
-            // todo 更新
+            _this.getIssue(_this.issue.issue_id)
           } else {
             _this.$toast(res.msg)
           }
