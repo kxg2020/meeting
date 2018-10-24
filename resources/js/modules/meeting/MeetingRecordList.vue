0c37196387e8856f601dc6567bde4b25f04f203f
lostinfo <1225039937@qq.com>
2018-10-24 15:20:03 +0800

    delete

--- resources/js/modules/meeting/MeetingRecordList.vue
+++ resources/js/modules/meeting/MeetingRecordList.vue
@@ -4,8 +4,8 @@
       <div v-if="meetingRecordList.length > 0">
         <div class="meeting-list">
           <!--todo 待参加会议提醒-->
-          <div v-for="(meetingItem, meetingIndex) in meetingRecordList" :key="meetingIndex" class="meeting-item" @click="toInfo(meetingItem.id)">
-            <div class="meeting-title">
+          <div v-for="(meetingItem, meetingIndex) in meetingRecordList" :key="meetingIndex" class="meeting-item">
+            <div class="meeting-title" @click="toInfo(meetingItem.id)">
               {{meetingItem.title}}
             </div>
             <div class="meeting-noti">
@@ -19,6 +19,10 @@
             <div class="meeting-noti">
               <span>结束时间</span>
               <span>{{meetingItem.end_time}}</span>
+            </div>
+            <div class="meeting-edit">
+              <el-button type="danger" size="mini" plain v-if="deleted" @click="deleteMeeting(meetingItem.id)">删除</el-button>
+              <el-button type="primary" size="mini" plain @click="toInfo(meetingItem.id)">查看</el-button>
             </div>
           </div>
         </div>
@@ -36,10 +40,6 @@
     <van-tabbar v-if="sponsored">
       <van-tabbar-item @click="toForm">
         <span>发起会议</span>
-        <i slot="icon" slot-scope="props" class="fa fa-edit"></i>
-      </van-tabbar-item>
-      <van-tabbar-item>
-        <span>按钮</span>
         <i slot="icon" slot-scope="props" class="fa fa-edit"></i>
       </van-tabbar-item>
     </van-tabbar>
@@ -62,7 +62,8 @@
         total: 0,
         hasMore: false,
         meetingRecordList: [],
-        sponsored: false
+        sponsored: false,
+        deleted: false,
       }
     },
     components: {
@@ -99,16 +100,43 @@
       getPermission() {
         this.axios.get('/permission/' + this.meeting_type_id).then(res => {
           this.sponsored = res.data.sponsored
+          this.deleted = res.data.deleted
         }).catch(error => {})
       },
       back () {
         this.$router.back()
       },
       toForm() {
+        if (!this.sponsored) {
+          this.$toast('没有权限')
+          return
+        }
         this.$router.push({path: '/meeting_record/create/' + this.meeting_type_id})
       },
       toInfo(id) {
         this.$router.push({path: '/meeting_record/info/' + id})
+      },
+      deleteMeeting(id) {
+        this.$dialog.confirm({
+          title: '提示',
+          message: '是否删除此会议'
+        }).then(() => {
+          this.axios.post('/meetingRecord/delete/' + id).then(res => {
+            if (res.status) {
+              this.$toast("删除成功")
+              setTimeout(() => {
+                this.page = 1
+                this.total = 0
+                this.hasMore = false
+                this.meetingRecordList = []
+                this.getMeetingList()
+              }, 2000)
+            } else {
+              this.$toast(res.msg)
+            }
+          })
+        }).catch(() => {
+        })
       }
     }
   }
@@ -127,7 +155,6 @@
   }
   .meeting-item {
     margin: 15px;
-    height: 150px;
     border: 1px solid #f1f4f8;
     border-radius: 5px;
     background-color: #FFFFFF;
@@ -151,5 +178,14 @@
   }
   .meeting-noti span:nth-child(2){
     width: 60%;
+  }
+  .meeting-edit{
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    padding: 10px 0;
+  }
+  .meeting-edit button{
+    margin-right: 20px;
   }
 </style>
