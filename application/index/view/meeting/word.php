4b2902bfdb67b4186d356db6e5e1e1658850f059
zhangtao <zhangtao@xinchao.mobi>
2018-10-29 16:27:07 +0800

    update

--- application/index/view/meeting/word.php
+++ application/index/view/meeting/word.php
@@ -2,11 +2,97 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>会议记录导出</title>
+    <title>导出</title>
+    <link rel="stylesheet" href="/static/css/bootstrap.css">
+    <link rel="stylesheet" href="/static/css/word.css">
 </head>
 <body>
-    <div>
-        
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="title">
+                    <h3>马儿扎哈马儿扎哈马儿扎哈马儿扎哈</h3>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="main">
+                    <table class="table table-bordered">
+                        <thead>
+                        <tr>
+                            <td>主持人:</td>
+                            <td>1</td>
+                        </tr>
+                        <tr>
+                            <td>应该参会人员:</td>
+                            <td>
+                                xxx、
+                                xxx、
+                                xxx、
+                                xxx、
+                                xxx、
+                                xxx、
+                                xxx、
+                                xxx、
+                                xxx、
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>实际参会人员:</td>
+                            <td>yyy</td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>阅知</td>
+                            <td>yyy</td>
+                        </tr>
+                        <tr>
+                            <td>表决</td>
+                            <td>yyy</td>
+                        </tr>
+                        <tr>
+                            <td>投票</td>
+                            <td>yyy</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div>
+                <div class="col-lg-12">
+                    <h4 style="color: red">第一项:xxxx</h4>
+                    <p>
+                        adadadadadadadadadadad
+                    </p>
+                    <img src="https://meeting.it9g.com/static/uploads/2018-10-25/zhangtao/ScreenShot2018_0524_223451279.jpg" alt="" style="height: 300px" width="100%">
+                    <h4 style="color: red">参与人员:</h4>
+                    张三、张三、张三、张三、张三、张三、张三、张三、张三、张三、
+                    张三、张三、张三、张三、张三、张三、张三、张三、张三、张三、
+                    张三、张三、张三、张三、张三、张三、张三、张三、张三、张三、
+                    张三、张三、张三、张三、张三、张三、张三、张三、张三、张三、
+                    张三、张三、张三、张三、张三、张三、张三、张三、张三、张三、
+                </div>
+            </div>
+            <div>
+                <div class="col-lg-12">
+                    <h4 style="color: red">第一项:xxxx</h4>
+                    <p>
+                        adadadadadadadadadadad
+                    </p>
+                    <img src="https://meeting.it9g.com/static/uploads/2018-10-25/zhangtao/ScreenShot2018_0524_223451279.jpg" alt="" style="height: 300px" width="100%">
+                    <h4 style="color: red">参与人员:</h4>
+                    张三、张三、张三、张三、张三、张三、张三、张三、张三、张三、
+                    张三、张三、张三、张三、张三、张三、张三、张三、张三、张三、
+                    张三、张三、张三、张三、张三、张三、张三、张三、张三、张三、
+                    张三、张三、张三、张三、张三、张三、张三、张三、张三、张三、
+                    张三、张三、张三、张三、张三、张三、张三、张三、张三、张三、
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
