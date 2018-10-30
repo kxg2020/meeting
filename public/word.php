5996b5c1060ec10f4212b6ac71e9f98389ac2aad
gaow <1225039937@qq.com>
2018-10-30 22:06:38 +0800

    word

---
+++ public/word.php
@@ -0,0 +1,173 @@
+<?php
+header("Cache-Control: no-cache, must-revalidate");
+header("Pragma: no-cache");
+header("Content-Type: application/octet-stream");
+header("Content-Disposition: attachment; filename=xxx.doc");
+$content = '
+<html xmlns:o="urn:schemas-microsoft-com:office:office" 
+            xmlns:w="urn:schemas-microsoft-com:office:word" 
+            xmlns="http://www.w3.org/TR/REC-html40">
+<head>
+    <meta charset="UTF-8">
+    <xml><w:WordDocument><w:View>Print</w:View></w:WordDocument></xml>
+    <title>月坝村第1次党员大会</title>
+    <style>
+        h1 {
+            text-align: center;
+            margin: 1em 0;
+        }
+
+        h2 {
+            margin: 1em 0;
+        }
+
+        td {
+            font-size: 18px;
+            line-height: 30px;
+            padding: 1em .5em;
+        }
+
+        .word {
+            font-size: 18px;
+            line-height: 22px;
+            text-indent: 2em;
+            margin: 1em 0;
+        }
+
+        .text-label {
+            color: #FF0000;
+        }
+        .img-box{
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+<div style="width: 100%;">
+    <h1>中共白朝乡月坝村党支部党员大会会议记录</h1>
+    <table border="1" style="width: 100%;">
+        <tr>
+            <td colspan="2">会标（时间）：月坝村第1次党员大会（2018.09.30）</td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <span>主  持  人：张某三</span>
+                <span>应参会人员：张某三、李某四、王某五、钱某六、赵某七、刘某九、何某十、孙十一</span>
+                <span>主  持  人：张某三</span>
+                <span>实际参会人员：张某三、李某四、王某五 </span>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 10%">阅知</td>
+            <td>第一项议题：学习镇党委政府文件</td>
+        </tr>
+        <tr>
+            <td style="width: 10%">阅知</td>
+            <td>第二项议题：通报7月份党建工作情况</td>
+        </tr>
+        <tr>
+            <td style="width: 10%">表决</td>
+            <td>第三项议题：讨论**同志预备党员转正</td>
+        </tr>
+        <tr>
+            <td style="width: 10%">投票</td>
+            <td>第四项议题：评选2018年三季度“好媳妇”</td>
+        </tr>
+    </table>
+    <div>
+        <h2><span style="color: #ff0000">第一项议题：</span>学习镇党委政府文件</h2>
+        <div class="word">
+            内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内
+            内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内
+            内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内
+            内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内
+            内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内
+        </div>
+        <div class="img-box">
+            <img style="width: 50%;"
+                 src="https://meeting.it9g.com/static/uploads/2018-10-29/whngqdcmhdxxf/8801FAA8-732D-48DD-B3D5-11C047D11B71.jpeg"
+                 alt="">
+            <img style="width: 50%;"
+                 src="https://meeting.it9g.com/static/uploads/2018-10-29/whngqdcmhdxxf/8801FAA8-732D-48DD-B3D5-11C047D11B71.jpeg"
+                 alt="">
+        </div>
+        <div><span class="text-label">阅读人员：</span><span>王某五、钱某六</span></div>
+    </div>
+    <div>
+        <h2><span style="color: #ff0000">第二项议题：</span>学习镇党委政府文件</h2>
+        <div class="word">
+            内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内
+            内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内
+        </div>
+        <div class="img-box">
+            <img style="width: 50%;"
+                 src="https://meeting.it9g.com/static/uploads/2018-10-29/whngqdcmhdxxf/8801FAA8-732D-48DD-B3D5-11C047D11B71.jpeg"
+                 alt="">
+        </div>
+        <div><span class="text-label">阅读人员：</span><span>王某五、钱某六</span></div>
+    </div>
+    <div>
+        <h2><span style="color: #ff0000">第三项议题：</span>讨论**同志预备党员转正</h2>
+        <div class="word">
+            内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内
+            内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内
+        </div>
+        <div class="img-box">
+            <img style="width: 50%;"
+                 src="https://meeting.it9g.com/static/uploads/2018-10-29/whngqdcmhdxxf/8801FAA8-732D-48DD-B3D5-11C047D11B71.jpeg"
+                 alt="">
+        </div>
+        <div>
+            <div><span class="text-label">同意：</span><span>10票</span></div>
+            <div><span class="text-label">反对：</span><span>5票</span></div>
+            <div><span class="text-label">弃权：</span><span>4票</span></div>
+            <div><span class="text-label">表决结果：</span><span>同意XX同志转正。</span></div>
+        </div>
+    </div>
+    <div>
+        <h2><span style="color: #ff0000">第三项议题：</span>评选2018年三季度“好媳妇”</h2>
+        <div class="word">
+            内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内
+            内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内
+        </div>
+        <div class="img-box">
+            <img style="width: 50%;"
+                 src="https://meeting.it9g.com/static/uploads/2018-10-29/whngqdcmhdxxf/8801FAA8-732D-48DD-B3D5-11C047D11B71.jpeg"
+                 alt="">
+        </div>
+        <div>
+            <div>
+                <div><span class="text-label">选项一：</span><span>张某</span></div>
+                <div class="word">
+                    张某说明张某说明张某说明张某说明张某说明张某说明张某说明张某说明
+                    张某说明张某说明张某说明张某说明张某说明张某说明张某说明张某说明
+                </div>
+                <div>
+                    <img style="width: 50%;"
+                         src="https://meeting.it9g.com/static/uploads/2018-10-29/whngqdcmhdxxf/8801FAA8-732D-48DD-B3D5-11C047D11B71.jpeg"
+                         alt="">
+                </div>
+            </div>
+            <div>
+                <div><span class="text-label">选项二：</span><span>李某</span></div>
+                <div class="word">
+                    李某说明李某说明李某说明李某说明李某说明
+                    李某说明李某说明李某说明李某说明李某说明
+                </div>
+                <div>
+                    <img style="width: 50%;"
+                         src="https://meeting.it9g.com/static/uploads/2018-10-29/whngqdcmhdxxf/8801FAA8-732D-48DD-B3D5-11C047D11B71.jpeg"
+                         alt="">
+                </div>
+            </div>
+        </div>
+        <div>
+            <div><span class="text-label">选项一：</span><span>张某</span><span>10票</span></div>
+            <div><span class="text-label">选项二：</span><span>李某</span><span>5票</span></div>
+            <div><span class="text-label">投票结果：</span><span>张某当选</span></div>
+        </div>
+    </div>
+</div>
+</body>
+</html>';
+echo $content;
