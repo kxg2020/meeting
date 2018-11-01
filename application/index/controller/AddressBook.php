c9268712e60bb42afc4ff2c774e6bae8b26ab45c
zhangtao <657920579@qq.com>
2018-11-01 20:58:15 +0800

    update

--- application/index/controller/AddressBook.php
+++ application/index/controller/AddressBook.php
@@ -21,10 +21,10 @@ class AddressBook extends Base{
     }
 
     private function validateToken($params){
-        $msgSignature  = urldecode($params["msg_signature"]);
-        $timestamp     = urldecode($params["timestamp"]);
-        $nonce         = urldecode($params["nonce"]);
-        $echoStr       = rawurldecode($params["echostr"]);
+        $msgSignature  = $params["msg_signature"];
+        $timestamp     = $params["timestamp"];
+        $nonce         = $params["nonce"];
+        $echoStr       = $params["echostr"];
         $array = [$nonce,$timestamp,$this->token,$echoStr];
         sort($array,SORT_STRING);
         $str = sha1(implode($array));
