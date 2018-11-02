fbe3222139e75342270ed08da366810e45f50bb1
zhangtao <zhangtao@xinchao.mobi>
2018-11-02 09:58:29 +0800

    update

--- application/index/controller/AddressBook.php
+++ application/index/controller/AddressBook.php
@@ -1,6 +1,10 @@
 <?php
 namespace app\index\controller;
 
+use app\index\service\Aes;
+use app\index\service\Pkcs;
+use app\index\service\WeChat;
+use think\Exception;
 use think\facade\Log;
 use think\facade\Request;
 
@@ -8,6 +12,8 @@ class AddressBook extends Base{
     private $encodeKey = "wqm1uwsSBxBLXE0lo5EUaru7lTUFSSWm0nD1bY3Kbmc";
     private $token     = "AddressBook";
     private $validate  = true;
+    private $iv;
+    private $key;
 
     /*
      * 通讯录变更
@@ -20,19 +26,41 @@ class AddressBook extends Base{
     }
 
     private function validateToken($params){
-        $msgSignature  = $params["msg_signature"];
-        $timestamp     = $params["timestamp"];
-        $nonce         = $params["nonce"];
-        $echoStr       = $params["echostr"];
+        $msgSignature  = rawurldecode($params["msg_signature"]);
+        $timestamp     = rawurldecode($params["timestamp"]);
+        $nonce         = rawurldecode($params["nonce"]);
+        $echoStr       = rawurldecode($params["echostr"]);
         $array = [$nonce,$timestamp,$this->token,$echoStr];
         sort($array,SORT_STRING);
         $str = sha1(implode($array));
         if($str == $msgSignature){
-            Log::error($str == $msgSignature);
-            Log::error($echoStr);
-            echo $echoStr;exit;
+            $this->key = base64_decode($this->encodeKey."=");
+            $this->iv  = substr($this->key, 0, 16);
+            $result = $this->decrypt($echoStr,WeChat::COMPANY_ID);
+            echo $result;exit;
         }else{
             $this->validate = false;
         }
     }
+
+    private function decrypt($encrypted, $receiveId){
+        $decrypted = (new Aes($this->key,"AES-256-CBC",$this->iv,OPENSSL_ZERO_PADDING))
+            ->decrypt($encrypted);
+        $pkc_encoder = new Pkcs();
+        $result      = $pkc_encoder->decode($decrypted);
+        if (strlen($result) < 16) {
+            return [];
+        }
+        $content     = substr($result, 16, strlen($result));
+        $len_list    = unpack('N', substr($content, 0, 4));
+        $xml_len     = $len_list[1];
+        $xmlContent  = substr($content, 4, $xml_len);
+        $from_receiveId = substr($content, $xml_len + 4);
+        if($from_receiveId != $receiveId){
+            return false;
+        }
+        return $xmlContent;
+    }
+
+
 }
