d6c8547fd58bd23b64d1195d192cb699ffa4ad78
lostinfo <1225039937@qq.com>
2018-11-14 14:41:19 +0800

    .

--- application/index/view/index/pdf.php
+++ application/index/view/index/pdf.php
@@ -3,41 +3,41 @@
 <head>
   <meta charset="UTF-8">
   <title>pdf template</title>
-<!--
-初号44pt
+  <!--
+  初号44pt
 
-小初36pt
+  小初36pt
 
-一号26pt
+  一号26pt
 
-小一24pt
+  小一24pt
 
-二号22pt
+  二号22pt
 
-小二18pt
+  小二18pt
 
-三号16pt
+  三号16pt
 
-小三15pt
+  小三15pt
 
-四号14pt
+  四号14pt
 
-小四12pt
+  小四12pt
 
-五号10.5pt
+  五号10.5pt
 
-小五9pt
+  小五9pt
 
-六号7.5pt
+  六号7.5pt
 
-小六6.5pt
+  小六6.5pt
 
-七号5.5pt
+  七号5.5pt
 
-八号5pt
-https://wenku.baidu.com/view/e2f6a0daa58da0116c1749dc
+  八号5pt
+  https://wenku.baidu.com/view/e2f6a0daa58da0116c1749dc
 
--->
+  -->
   <style>
     body {
       font-size: 14pt;
@@ -98,7 +98,7 @@ https://wenku.baidu.com/view/e2f6a0daa58da0116c1749dc
       font-size: 16pt;
       font-family: "FangSong";
       color: #000000;
-        page-break-before: always;
+      page-break-before: always;
     }
 
     .main-title {
@@ -111,45 +111,54 @@ https://wenku.baidu.com/view/e2f6a0daa58da0116c1749dc
       font-weight: bold;
 
     }
+
     .content-block {
       text-indent: 2em;
     }
-    .enclosure{
+
+    .enclosure {
       text-indent: 2em;
     }
-    .enclosure-more{
+
+    .enclosure-more {
       text-indent: 5em;
     }
-    .enclosure-item-no{
+
+    .enclosure-item-no {
       font-weight: bold;
       color: #000000;
     }
-    .enclosure-item{
-        page-break-before: always;
+
+    .enclosure-item {
+      page-break-before: always;
     }
 
     /**/
-    table{
+    table {
       width: 100%;
       border-collapse: collapse;
       border: 2px solid red;
     }
-    thead{
+
+    thead {
       font-weight: bold;
       font-size: 14pt;
     }
-    tbody{
+
+    tbody {
       font-size: 12pt;
     }
-    td, th{
+
+    td, th {
       border: 2px solid #000000;
-        height: 80px;
-        font-weight: bolder;
-        font-size: 20px;
+      height: 80px;
+      font-weight: bolder;
+      font-size: 20px;
     }
-    table tr:nth-child(2) td{
-         height: 270px;
-     }
+
+    table tr:nth-child(2) td {
+      height: 270px;
+    }
 
   </style>
 </head>
@@ -161,94 +170,113 @@ https://wenku.baidu.com/view/e2f6a0daa58da0116c1749dc
     <div class="header-number">中共白朝乡〔2018〕60 号</div>
     <div class="header-hr"></div>
   </div>
-    <div class="empty-block"></div>
-    <div class="empty-block"></div>
-    <table border="1" style="width: 100%;" cellpadding="0" cellspacing="0">
-        <tr>
-            <td colspan="2"><strong>会标（时间）</strong>：2018-11-14 </td>
-        </tr>
-        <tr>
-            <td colspan="2">
-                <span><strong>主持人</strong>：顶顶顶</span>
-                <br>
-                <span>
+  <div class="empty-block"></div>
+  <div class="empty-block"></div>
+  <table border="1" style="width: 100%;" cellpadding="0" cellspacing="0">
+    <tr>
+      <td colspan="2"><strong>会标（时间）</strong>：2018-11-14</td>
+    </tr>
+    <tr>
+      <td colspan="2">
+        <span><strong>主持人</strong>：顶顶顶</span>
+        <br>
+        <span>
                       <strong>应参会人员</strong> ：按时大苏打、按时大苏打、、按时大苏打、、按时大苏打、、按时大苏打、、按时大苏打、、按时大苏打、、按时大苏打、、按时大苏打、、按时大苏打、、按时大苏打、
                   </span>
-                <br>
-                <span>
+        <br>
+        <span>
                      <strong>实际参会人员</strong> ：烦烦烦、按时大苏打、按时大苏打、按时大苏打、按时大苏打、按时大苏打、按时大苏打、、按时大苏打、按时大苏打、按时大苏打、按时大苏打、按时大苏、按时大苏打、按时大苏打、按时大苏打、</span>
-            </td>
-        </tr>
-        <tr>
-            <td style="width: 10%;"><strong>阅知</strong></td>
-            <td><strong>测试与之</strong></td>
-        </tr>
-        <tr>
-            <td style="width: 10%"><strong>表决</strong></td>
-            <td><strong>测试表决</strong></td>
-        </tr>
-        <tr>
-            <td style="width: 10%"><strong>投票</strong></td>
-            <td><strong>测试投票</strong></td>
-        </tr>
-    </table>
-    <div class="main">
-              <div class="content-cell"><strong>第一项议题:传道受业解惑</strong></div>
-             <div class="empty-block"></div>
-
-              <div class="content-block">
-                  古之学者必有师。师者，所以传道受业解惑也。人非生而知之者，孰能无惑？惑而不从师，其为惑也，终不解矣。生乎吾前，其闻道也固先乎吾，吾从而师之；生乎吾后，其闻道也亦先乎吾，吾从而师之。吾师道也，夫庸知其年之先后生于吾乎？是故无贵无贱，无长无少，道之所存，师之所存也。
-              </div>
-              <div class="content-block">
-                  嗟（jiē）乎！师道之不传也久矣！欲人之无惑也难矣！古之圣人，其出人也远矣，犹且从师而问焉；今之众人，其下圣人也亦远矣，而耻学于师。是故圣益圣，愚益愚。圣人之所以为圣，愚人之所以为愚，其皆出于此乎！爱其子，择师而教之；于其身也，则耻师焉，惑矣。彼童子之师，授之书而习其句读（dòu）者，非吾所谓传其道解其惑者也。句读之不知，惑之不解，或师焉，或不（fǒu）焉，小学而大遗，吾未见其明也。巫医乐师百工之人，不耻相师。士大夫之族，曰师曰弟子云者，则群聚而笑之。问之，则曰：“彼与彼年相若也，道相似也。位卑则足羞，官盛则近谀。”呜呼！师道之不复，可知矣。巫医乐师百工之人，君子不齿，今其智乃反不能及，其可怪也欤！
-              </div>
-              <div class="content-block">
-                  圣人无常师。孔子师郯（tán）子、苌弘、师襄、老聃（dān）。郯子之徒，其贤不及孔子。孔子曰：三人行，则必有我师。是故弟子不必不如师，师不必贤于弟子，闻道有先后，术业有专攻，如是而已。
-              </div>
-              <div class="content-block">
-                  李氏子蟠，年十七，好古文，六艺经传皆通习之，不拘于时，学于余。余嘉其能行古道，作《师说》以贻之
-              </div>
-
+      </td>
+    </tr>
+    <tr>
+      <td style="width: 10%;"><strong>阅知</strong></td>
+      <td><strong>测试与之</strong></td>
+    </tr>
+    <tr>
+      <td style="width: 10%"><strong>表决</strong></td>
+      <td><strong>测试表决</strong></td>
+    </tr>
+    <tr>
+      <td style="width: 10%"><strong>投票</strong></td>
+      <td><strong>测试投票</strong></td>
+    </tr>
+  </table>
+  <div class="main">
+    <div class="main-title"><strong>第一项议题:传道受业解惑</strong></div>
+    <div class="empty-block"></div>
+    <div class="content-block">
+      古之学者必有师。师者，所以传道受业解惑也。人非生而知之者，孰能无惑？惑而不从师，其为惑也，终不解矣。生乎吾前，其闻道也固先乎吾，吾从而师之；生乎吾后，其闻道也亦先乎吾，吾从而师之。吾师道也，夫庸知其年之先后生于吾乎？是故无贵无贱，无长无少，道之所存，师之所存也。
     </div>
-    <div class="main">
-        <div class="content-cell">第一项议题:传道受业解惑</div>
-        <div class="empty-block"></div>
-        <div class="content-block">
-            古之学者必有师。师者，所以传道受业解惑也。人非生而知之者，孰能无惑？惑而不从师，其为惑也，终不解矣。生乎吾前，其闻道也固先乎吾，吾从而师之；生乎吾后，其闻道也亦先乎吾，吾从而师之。吾师道也，夫庸知其年之先后生于吾乎？是故无贵无贱，无长无少，道之所存，师之所存也。
-        </div>
-        <div class="content-block">
-            嗟（jiē）乎！师道之不传也久矣！欲人之无惑也难矣！古之圣人，其出人也远矣，犹且从师而问焉；今之众人，其下圣人也亦远矣，而耻学于师。是故圣益圣，愚益愚。圣人之所以为圣，愚人之所以为愚，其皆出于此乎！爱其子，择师而教之；于其身也，则耻师焉，惑矣。彼童子之师，授之书而习其句读（dòu）者，非吾所谓传其道解其惑者也。句读之不知，惑之不解，或师焉，或不（fǒu）焉，小学而大遗，吾未见其明也。巫医乐师百工之人，不耻相师。士大夫之族，曰师曰弟子云者，则群聚而笑之。问之，则曰：“彼与彼年相若也，道相似也。位卑则足羞，官盛则近谀。”呜呼！师道之不复，可知矣。巫医乐师百工之人，君子不齿，今其智乃反不能及，其可怪也欤！
-        </div>
-        <div class="content-block">
-            圣人无常师。孔子师郯（tán）子、苌弘、师襄、老聃（dān）。郯子之徒，其贤不及孔子。孔子曰：三人行，则必有我师。是故弟子不必不如师，师不必贤于弟子，闻道有先后，术业有专攻，如是而已。
-        </div>
-        <div class="content-block">
-            李氏子蟠，年十七，好古文，六艺经传皆通习之，不拘于时，学于余。余嘉其能行古道，作《师说》以贻之
-        </div>
+    <div class="content-block">
+      嗟（jiē）乎！师道之不传也久矣！欲人之无惑也难矣！古之圣人，其出人也远矣，犹且从师而问焉；今之众人，其下圣人也亦远矣，而耻学于师。是故圣益圣，愚益愚。圣人之所以为圣，愚人之所以为愚，其皆出于此乎！爱其子，择师而教之；于其身也，则耻师焉，惑矣。彼童子之师，授之书而习其句读（dòu）者，非吾所谓传其道解其惑者也。句读之不知，惑之不解，或师焉，或不（fǒu）焉，小学而大遗，吾未见其明也。巫医乐师百工之人，不耻相师。士大夫之族，曰师曰弟子云者，则群聚而笑之。问之，则曰：“彼与彼年相若也，道相似也。位卑则足羞，官盛则近谀。”呜呼！师道之不复，可知矣。巫医乐师百工之人，君子不齿，今其智乃反不能及，其可怪也欤！
     </div>
-    <div class="main">
-                      <div class="enclosure">附件：1.六艺经传皆通习之</div>
-                      <div class="enclosure-more">2.所以传道受业解惑也</div>
-                      <div class="enclosure-more">3.所以传道受业解惑也</div>
-                      <div class="empty-block h3em"></div>
+    <div class="content-block">
+      圣人无常师。孔子师郯（tán）子、苌弘、师襄、老聃（dān）。郯子之徒，其贤不及孔子。孔子曰：三人行，则必有我师。是故弟子不必不如师，师不必贤于弟子，闻道有先后，术业有专攻，如是而已。
     </div>
-    <div class="enclosure-item">
-        <div class="enclosure-item-no">附件一</div>
-        <div class="main-title">所以传道受业解惑也</div>
-        <div class="empty-block h2em"></div>
-        <div class="content-block">
-            <img style="width: 90%; height:400px; display: block;" src="https://meeting.it9g.com/static/uploads/2018-10-25/zhangtao/ScreenShot2018_0524_223451279.jpg" alt="">
-        </div>
+    <div class="content-block">
+      李氏子蟠，年十七，好古文，六艺经传皆通习之，不拘于时，学于余。余嘉其能行古道，作《师说》以贻之
     </div>
+    <div class="h1em"></div>
+    <div class="vote-list">
+      <div class="vote-title">
+        <strong>关于xxxx的投票</strong>
+      </div>
+      <div class="vote-item">
+        <span>选项一、</span><strong>张三</strong><span>12票</span>
+      </div>
+      <div class="vote-item">
+        <span>选项二、</span><strong>李四</strong><span>120票</span>
+      </div>
+      <div class="vote-item">
+        <span>选项三、</span><strong>王五</strong><span>1200票</span>
+      </div>
+      <div class="vote-item">
+        <span>投票结果</span><strong>王五</strong>
+      </div>
+    </div>
+    <div class="h1em"></div>
+  </div>
+  <div class="main">
+    <div class="main-title"><strong>第一项议题:传道受业解惑</strong></div>
+    <div class="empty-block"></div>
+    <div class="content-block">
+      古之学者必有师。师者，所以传道受业解惑也。人非生而知之者，孰能无惑？惑而不从师，其为惑也，终不解矣。生乎吾前，其闻道也固先乎吾，吾从而师之；生乎吾后，其闻道也亦先乎吾，吾从而师之。吾师道也，夫庸知其年之先后生于吾乎？是故无贵无贱，无长无少，道之所存，师之所存也。
+    </div>
+    <div class="content-block">
+      嗟（jiē）乎！师道之不传也久矣！欲人之无惑也难矣！古之圣人，其出人也远矣，犹且从师而问焉；今之众人，其下圣人也亦远矣，而耻学于师。是故圣益圣，愚益愚。圣人之所以为圣，愚人之所以为愚，其皆出于此乎！爱其子，择师而教之；于其身也，则耻师焉，惑矣。彼童子之师，授之书而习其句读（dòu）者，非吾所谓传其道解其惑者也。句读之不知，惑之不解，或师焉，或不（fǒu）焉，小学而大遗，吾未见其明也。巫医乐师百工之人，不耻相师。士大夫之族，曰师曰弟子云者，则群聚而笑之。问之，则曰：“彼与彼年相若也，道相似也。位卑则足羞，官盛则近谀。”呜呼！师道之不复，可知矣。巫医乐师百工之人，君子不齿，今其智乃反不能及，其可怪也欤！
+    </div>
+    <div class="content-block">
+      圣人无常师。孔子师郯（tán）子、苌弘、师襄、老聃（dān）。郯子之徒，其贤不及孔子。孔子曰：三人行，则必有我师。是故弟子不必不如师，师不必贤于弟子，闻道有先后，术业有专攻，如是而已。
+    </div>
+    <div class="content-block">
+      李氏子蟠，年十七，好古文，六艺经传皆通习之，不拘于时，学于余。余嘉其能行古道，作《师说》以贻之
+    </div>
+  </div>
+  <div class="main">
+    <div class="enclosure">附件：1.六艺经传皆通习之</div>
+    <div class="enclosure-more">2.所以传道受业解惑也</div>
+    <div class="enclosure-more">3.所以传道受业解惑也</div>
     <div class="empty-block h3em"></div>
-    <div class="enclosure-item">
-        <div class="enclosure-item-no">附件二</div>
-        <div class="main-title">所以传道受业解惑也</div>
-        <div class="empty-block h2em"></div>
-        <div class="content-block">
-            <img style="width: 90%; height:400px; display: block;" src="https://meeting.it9g.com/static/uploads/2018-10-25/zhangtao/ScreenShot2018_0524_223451279.jpg" alt="">
-        </div>
+  </div>
+  <div class="enclosure-item">
+    <div class="enclosure-item-no">附件一</div>
+    <div class="main-title">所以传道受业解惑也</div>
+    <div class="empty-block h2em"></div>
+    <div class="content-block">
+      <img style="width: 90%; height:400px; display: block;"
+           src="https://meeting.it9g.com/static/uploads/2018-10-25/zhangtao/ScreenShot2018_0524_223451279.jpg" alt="">
     </div>
+  </div>
+  <div class="empty-block h3em"></div>
+  <div class="enclosure-item">
+    <div class="enclosure-item-no">附件二</div>
+    <div class="main-title">所以传道受业解惑也</div>
+    <div class="empty-block h2em"></div>
+    <div class="content-block">
+      <img style="width: 90%; height:400px; display: block;"
+           src="https://meeting.it9g.com/static/uploads/2018-10-25/zhangtao/ScreenShot2018_0524_223451279.jpg" alt="">
+    </div>
+  </div>
 </div>
 </div>
 </body>
